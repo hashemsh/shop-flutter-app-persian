1aade529e1121a0ff3d430433c057f36d65845a5
hashem sheikhypour <sh.sheikhypour@gmail.com>
2023-01-30 00:25:29 +0330

    counter of product and functional

---
+++ lib/screens/details/components/cart_counter.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app_flutter/constants.dart';
+
+class CartCounter extends StatefulWidget {
+  const CartCounter({super.key});
+
+  @override
+  State<CartCounter> createState() => _CartCounterState();
+}
+
+class _CartCounterState extends State<CartCounter> {
+  int numOfItem = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        buildOutlineButton(
+          icon: Icons.remove,
+          press: (() {
+            if (numOfItem > 1) {
+              setState(
+                () {
+                  numOfItem--;
+                },
+              );
+            }
+          }),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding / 2),
+          child: Text(
+            numOfItem.toString().padLeft(2, "0"),
+            style: Theme.of(context)
+                .textTheme
+                .headline6!
+                .copyWith(fontWeight: FontWeight.bold),
+          ),
+        ),
+        buildOutlineButton(
+          icon: Icons.add,
+          press: (() {
+            setState(
+              () {
+                numOfItem++;
+              },
+            );
+          }),
+        ),
+      ],
+    );
+  }
+
+  SizedBox buildOutlineButton(
+      {required IconData icon, required VoidCallback press}) {
+    return SizedBox(
+      width: 40,
+      height: 32,
+      child: OutlinedButton(
+        style: OutlinedButton.styleFrom(
+          padding: EdgeInsets.zero,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(13),
+          ),
+        ),
+        onPressed: press,
+        child: Icon(
+          icon,
+          color: kTextColor,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/details/components/description.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app_flutter/constants.dart';
+import 'package:shop_app_flutter/models/Product.dart';
+
+class Description extends StatelessWidget {
+  const Description({
+    Key? key,
+    required this.product,
+  }) : super(key: key);
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: kDefaultPadding),
+      child: Text(
+        product.description,
+        style: const TextStyle(height: 2),
+      ),
+    );
+  }
+}
