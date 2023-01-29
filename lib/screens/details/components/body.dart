146575fac8e94372c744363ab675252015810722
hashem sheikhypour <sh.sheikhypour@gmail.com>
2023-01-30 00:24:36 +0330

    make product detail header like image, title ,etc

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app_flutter/constants.dart';
 import 'package:shop_app_flutter/models/Product.dart';
+import 'package:shop_app_flutter/screens/details/components/cart_counter.dart';
+import 'package:shop_app_flutter/screens/details/components/color_and_size.dart';
+import 'package:shop_app_flutter/screens/details/components/description.dart';
+import 'package:shop_app_flutter/screens/details/components/product_details_header.dart';
 
 class Body extends StatelessWidget {
   final Product product;
@@ -19,67 +23,27 @@ class Body extends StatelessWidget {
               children: [
                 Container(
                   margin: EdgeInsets.only(top: size.height * 0.3),
-                  height: 500,
+                  padding: EdgeInsets.only(
+                    top: size.height * 0.12,
+                    left: kDefaultPadding,
+                    right: kDefaultPadding,
+                  ),
                   decoration: const BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(24),
-                          topRight: Radius.circular(24))),
-                ),
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(24),
+                      topRight: Radius.circular(24),
+                    ),
+                  ),
                   child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(
-                        'کیف های جذاب و دیدنی ',
-                        style: theme.subtitle2?.copyWith(color: Colors.white),
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      Text(
-                        product.title,
-                        style: theme.headline4?.copyWith(
-                            color: Colors.white, fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(
-                        height: kDefaultPadding,
-                      ),
-                      Row(
-                        children: [
-                          RichText(
-                            text: TextSpan(
-                              children: [
-                                TextSpan(
-                                    text: "قیمت\n",
-                                    style: theme.subtitle2!
-                                        .copyWith(color: Colors.white)),
-                                TextSpan(
-                                  text: "${product.price} تومان",
-                                  style: theme.headline4?.copyWith(
-                                      color: Colors.white,
-                                      fontSize: 22,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                              ],
-                            ),
-                          ),
-                          const SizedBox(
-                            width: kDefaultPadding,
-                          ),
-                          Expanded(
-                            child: Image.asset(
-                              product.image,
-                              fit: BoxFit.fill,
-                            ),
-                          ),
-                        ],
-                      )
+                      ColorAndSize(theme: theme, product: product),
+                      Description(product: product),
+                      CartCounter(),
                     ],
                   ),
-                )
+                ),
+                ProductDetailsHeader(theme: theme, product: product)
               ],
             ),
           ),
---
+++ lib/screens/details/components/product_details_header.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app_flutter/constants.dart';
+import 'package:shop_app_flutter/models/Product.dart';
+
+class ProductDetailsHeader extends StatelessWidget {
+  const ProductDetailsHeader({
+    Key? key,
+    required this.theme,
+    required this.product,
+  }) : super(key: key);
+
+  final TextTheme theme;
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'کیف های جذاب و دیدنی ',
+            style: theme.subtitle2?.copyWith(color: Colors.white),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+            product.title,
+            style: theme.headline4
+                ?.copyWith(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: kDefaultPadding,
+          ),
+          Row(
+            children: [
+              RichText(
+                text: TextSpan(
+                  children: [
+                    TextSpan(
+                        text: "قیمت\n",
+                        style: theme.subtitle2!.copyWith(color: Colors.white)),
+                    TextSpan(
+                      text: "${product.price} تومان",
+                      style: theme.headline4?.copyWith(
+                          color: Colors.white,
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                width: kDefaultPadding,
+              ),
+              Expanded(
+                child: Image.asset(
+                  product.image,
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
